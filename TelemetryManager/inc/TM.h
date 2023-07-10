d8aa14901743e7133352f47ae3fed53b316c4043
billylin609 <y29lin@uwaterloo.ca>
2023-07-10 14:33:48 -0400

    add TM header file

---
+++ TelemetryManager/inc/TM.h
@@ -0,0 +1,49 @@
+/*
+ * TM.h
+ *
+ * Telemtry Manager Header
+ *
+ * Created on: Jul 10, 2023
+ */
+
+#ifndef ZPSW3_5_TM_H
+#define ZPSW3_5_TM_H
+
+#define E_OK 1
+#define E_NOT_OK 0
+
+typedef uint8_t Std_ReturnType;
+
+namespace TM {
+	class TelemetryManager {
+		public:
+			/**
+			 * TM constructor with assigned telemtry port
+			 * init generic uart driver in constructor
+			 *
+			 * @param _huart uart port assigned
+			 */
+			TelemetryManger(UART_HandlerTypeDef* _huart)
+				:
+				_uart_handler(_huart) {};
+
+			/*transmit mavlink packet interface function starts here*/
+
+			/*transmit mavlink packet interface function ends here*/
+			
+
+			/*receive mavlink packet interface function starts here*/
+				Std_ReturnType Mavlink_Recieve(MavlinkMessage* buf) {
+					/*call uart driver here*/
+				}		
+			/*receive mavlink packet interface function ends here*/
+		private:
+			/*mavlink library public function starts here*/
+
+			/*mavlink library public function ends here*/
+
+			
+			UART_HandlerTypeDef* _uart_handler;
+	}
+}
+#endif //ZPSW3_5_TM_H
